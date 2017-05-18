239c596e13330741d746f3f1c4e31aec25cd98a5
mnawakining <kav230@mail.ru>
2017-05-19 01:07:33 +0300

    Create string, upper and lower, len.c

---
+++ string, upper and lower, len.c
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[])
+{    
+    int i=0;
+    char str[30];
+    
+    scanf("%s", &str);
+    if (strcmp(str,"quit") == 0) return 0;//сравнивание строк
+    else
+    {
+        printf("String lenght: %d \n", strlen(str));//вывод длины строки
+        while(str[i] != '\0')//пока не встр. символ окончания строки
+        {
+            putchar(tolower(str[i])); //преобразование символа к нижнему регистру и вывод символа по коду
+            i++;
+        }
+        i = 0;
+        printf("\n");
+
+        while(str[i] != '\0')
+        {   putchar(toupper(str[i]));
+            i++;
+        }
+        printf("\n");
+        i = 0;
+        while(str[i] != '\0')//пока не встр. символ окончания строки
+        {
+            if (i == 0 || str[i-1] == '.') putchar(toupper(str[i]));
+            else putchar(tolower(str[i])); 
+            //printf("%s",s);
+            i++;
+        }
+        i = 0;
+    }
+    return 0;
+}
