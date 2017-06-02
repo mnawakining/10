5f17d2210fa0dc9e50fd6006961d03f56d14dabb
mnawakining <kav230@mail.ru>
2017-06-02 16:34:10 +0300

    Update gen.c

--- gen.c
+++ gen.c
@@ -6,12 +6,11 @@ int main(int argc, char *argv[])
 {
 
 FILE *f;//обявляем переменую файл
-char fileName[20] = "genome.txt";
-char str[600000] = "", s[6];
-char beg[6] = "TTGACA", end[6] = "TATAAT";
-int i=0, ibeg=0, iend=0, flag =0, g = 0, countH = 0;
+char fileName[20] = "u.txt";
+char str[600000] = "";
+int i=0, ibeg=0, iend=0, flag =0, g = 0, H = 0, j = 0;
 
-
+    scanf("%d",&H);
 
     f = fopen(fileName, "r"); //открываем файл с путем fileName, для чтения
     if (f == NULL) printf("%s", "Empty file!\n"); //если файл не удалось открыть файл, выводим сообщение об ошибке
@@ -37,20 +36,19 @@ int i=0, ibeg=0, iend=0, flag =0, g = 0, countH = 0;
                   flag = 1;
                   iend = i;
                   i += 9;
+                  printf("String: \n");
                 }
                 i++;
                 g++;
             }
 
         }
-        if (flag == 1 && str[i] == 'H') countH++;
-
+        if (flag == 1 && j < H) {
+            printf("%c",str[i]);
+            j++;}
     }
-
-
-    printf("Begin : %d\n End: %d\n Count of H: %d", ibeg, iend, countH);
-
-
+printf("\nBegin : %d\nEnd: %d\n", ibeg, iend);
 return 0;
 
 }
+
